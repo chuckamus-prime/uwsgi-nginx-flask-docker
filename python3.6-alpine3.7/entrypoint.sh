909732270b6d76e2329cf33a3caa5bdfd9fa96e8
chuckamus-prime <chuck.sailors@advance-auto.com>
2018-07-06 13:11:01 -0400

    removed more references about static files, moved settings for nginx from entrypoint to regular nginx files.

--- python3.6-alpine3.7/entrypoint.sh
+++ python3.6-alpine3.7/entrypoint.sh
@@ -21,38 +21,11 @@ if [[ -v NGINX_WORKER_OPEN_FILES ]] ; then
     echo "worker_rlimit_nofile ${NGINX_WORKER_OPEN_FILES};" >> /etc/nginx/nginx.conf
 fi
 
-# Get the URL for static files from the environment variable
-USE_STATIC_URL=${STATIC_URL:-'/static'}
-# Get the absolute path of the static files from the environment variable
-USE_STATIC_PATH=${STATIC_PATH:-'/app/static'}
-# Get the listen port for Nginx, default to 80
+# Get the listen port for Nginx, default to 443
 USE_LISTEN_PORT=${LISTEN_PORT:-443}
 
 # Explicitly add installed Python packages and uWSGI Python packages to PYTHONPATH
 # Otherwise uWSGI can't import Flask
 export PYTHONPATH=$PYTHONPATH:/usr/local/lib/python3.6/site-packages:/usr/lib/python3.6/site-packages
-
-# Generate Nginx config first part using the environment variables
-echo "server {
-    listen ${USE_LISTEN_PORT};
-    location / {
-        try_files \$uri @app;
-    }
-    location @app {
-        include uwsgi_params;
-        uwsgi_pass unix:///tmp/uwsgi.sock;
-    }
-    location $USE_STATIC_URL {
-        alias $USE_STATIC_PATH;
-    }" > /etc/nginx/conf.d/nginx.conf
-
-# If STATIC_INDEX is 1, serve / with /static/index.html directly (or the static URL configured)
-if [[ $STATIC_INDEX == 1 ]] ; then
-echo "    location = / {
-        index $USE_STATIC_URL/index.html;
-    }" >> /etc/nginx/conf.d/nginx.conf
-fi
-# Finish the Nginx config file
-echo "}" >> /etc/nginx/conf.d/nginx.conf
 
 exec "$@"
