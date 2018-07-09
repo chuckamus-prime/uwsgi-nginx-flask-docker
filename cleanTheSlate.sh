49e1b0593c48ce9815e5251565f0fe156dca0f02
chuckamus-prime <chuck.sailors@advance-auto.com>
2018-07-09 08:53:14 -0400

    added a script to tear down the container and remove the images completely.

---
+++ cleanTheSlate.sh
@@ -0,0 +1,4 @@
+docker stop example-flask-python36-alpine37_web_1
+docker rm example-flask-python36-alpine37_web_1
+docker rmi example-flask-python36-alpine37_web:latest
+dcoker rmi chuckamus-prime/uwsgi-nginx-flask:python3.6-alpine3.7
